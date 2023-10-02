9b9b305b4d6a9705017ff1cb5764267f2d66ba11
Ramilbtc <ramadjamal@yandex.ru>
2023-10-02 06:46:43 +0300

    c2

--- contracts/GPTMembership.sol
+++ contracts/GPTMembership.sol
@@ -1,6 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-
-pragma solidity ^0.8.7;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
